3fb9fd8a8f9096d5731c09e67df1ee10eaa61a67
zhaopeng <zhaopeng026@163.com>
2018-08-07 23:11:21 +0800

    semaphore test

---
+++ thread/semaphore/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <semaphore.h>
+#include <pthread.h>
+#include <unistd.h>
+
+sem_t sem;
+
+void *thread1(void *arg)
+{
+    while(1) {
+        sleep(1);
+        sem_post(&sem);
+    }   
+    return NULL;
+}
+
+void *thread2(void *arg)
+{
+    while(1) {
+        printf("before wait!\r\n");
+        sem_wait(&sem);
+        printf("after wait!\r\n");
+    }
+    return NULL;
+}
+
+int main(int argc, char ** argv)
+{
+    pthread_t tid1, tid2;
+    
+    sem_init(&sem, 0, 1);
+    
+    pthread_create(&tid1, NULL, thread1, NULL);
+    pthread_create(&tid2, NULL, thread2, NULL);
+
+    pthread_join(tid1, NULL);
+    pthread_join(tid2, NULL);
+
+
+    sem_destroy(&sem);
+}
