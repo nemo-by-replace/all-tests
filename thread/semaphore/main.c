5e8e0d830252f50434701ae7f82074c87aecda53
nemo <zhaopeng026@163.com>
2018-08-08 23:15:11 +0800

    thread-signal

--- thread/semaphore/main.c
+++ thread/semaphore/main.c
@@ -2,6 +2,8 @@
 #include <semaphore.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <errno.h>
+#include <string.h>
 
 sem_t sem;
 
@@ -24,18 +26,32 @@ void *thread2(void *arg)
     return NULL;
 }
 
+
+void *thread3(void *arg)
+{
+    while(1) {
+        int ret = sem_trywait(&sem);
+        if(!ret)
+             printf("trywait:%d, %d, %s\r\n", ret, errno, strerror(errno));
+        
+        usleep(100000);
+    }
+    return NULL;
+}
+
 int main(int argc, char ** argv)
 {
-    pthread_t tid1, tid2;
+    pthread_t tid1, tid2, tid3;
     
     sem_init(&sem, 0, 1);
     
     pthread_create(&tid1, NULL, thread1, NULL);
     pthread_create(&tid2, NULL, thread2, NULL);
+    pthread_create(&tid3, NULL, thread3, NULL);
 
     pthread_join(tid1, NULL);
     pthread_join(tid2, NULL);
-
+    pthread_join(tid3, NULL);
 
     sem_destroy(&sem);
 }
---
+++ thread/thread-signal/main.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <semaphore.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/types.h>
+
+sem_t sem;
+
+void sig_handler(int sig)
+{
+    printf("thread id is:%ld\r\n", pthread_self());//thread id is equal to the main-thread's id
+    printf("%s\r\n", __FUNCTION__);
+}
+
+void sig_handler1(int sig)
+{
+    printf("thread id is:%ld\r\n", pthread_self());//thread id is equal to the main-thread's id
+    printf("%s\r\n", __FUNCTION__);
+}
+
+void sig_handler2(int sig)
+{
+    printf("thread id is:%ld\r\n", pthread_self());//thread id is equal to the main-thread's id
+    printf("%s\r\n", __FUNCTION__);
+}
+
+void *thread1(void *arg)
+{
+    signal(SIGALRM, sig_handler);
+    printf("%s in, pid:%ld\r\n", __FUNCTION__, getpid());
+    pause();    
+    printf("%s out\r\n", __FUNCTION__);
+    return NULL;
+}
+
+void *thread2(void *arg)
+{
+    signal(SIGALRM, sig_handler1);
+    printf("%s in, pid:%ld\r\n", __FUNCTION__, getpid());
+    while(1) {
+        sleep(1);
+        alarm(1);    
+    }
+    pause();    
+    printf("%s out\r\n", __FUNCTION__);
+    return NULL;
+}
+
+
+void *thread3(void *arg)
+{
+    signal(SIGALRM, sig_handler2);
+    printf("%s in, pid:%ld\r\n", __FUNCTION__, getpid());
+    pause();    
+    printf("%s out\r\n", __FUNCTION__);
+    return NULL;
+}
+
+int main(int argc, char ** argv)
+{
+    pthread_t tid1, tid2, tid3;
+    
+    printf("main thread id is %ld\r\n", pthread_self());
+    
+    signal(SIGALRM, SIG_IGN);
+    pthread_create(&tid1, NULL, thread1, NULL);
+    pthread_create(&tid2, NULL, thread2, NULL);
+    pthread_create(&tid3, NULL, thread3, NULL);
+    
+    printf("tid1:%ld, tid2:%ld, tid3:%ld\r\n", tid1, tid2, tid3);
+/*
+    alarm(1);
+    alarm(2); 
+    pause();
+    printf("start join\r\n");
+    alarm(1);
+*/
+/*
+    sleep(2);
+    pthread_kill(tid1, SIGALRM);
+    pthread_kill(tid2, SIGALRM);
+    pthread_kill(tid3, SIGALRM);
+*/
+/*
+    while(1) {
+        sleep(1);    
+        alarm(1);
+    }
+*/
+    
+    pthread_join(tid1, NULL);
+    pthread_join(tid2, NULL);
+    pthread_join(tid3, NULL);
+    
+    return 0;
+}
+
