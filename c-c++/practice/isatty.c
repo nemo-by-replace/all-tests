8b6103e693f019527665efae7c3c647e144218d1
nemo <zhaopeng026@163.com>
2018-08-15 19:56:34 +0800

    modify isatty.c

--- c-c++/practice/isatty.c
+++ c-c++/practice/isatty.c
@@ -3,7 +3,15 @@
 
 int main(int argc, char **argv)
 {
+    char buf[1024];
+    int n;
+
     printf("stdin is %d\r\n", isatty(0));
     printf("stdout is %d\r\n", isatty(1));
+    
+    n = read(0, buf, 1024);
+    printf("n:%d\r\n", n);
+
+    printf("%s\r\n", buf);
     return 0;
 }
