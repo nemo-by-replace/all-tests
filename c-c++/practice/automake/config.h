4409bf550048a4bc683446d7aa92bd41f37f8f6d
nemo <zhaopeng026@163.com>
2019-03-12 12:07:23 +0000

    a simple automake

---
+++ c-c++/practice/automake/config.h
@@ -0,0 +1,26 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "Hellocsk"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "Chen_shikun@163.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "helo"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "helo 1.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "helo"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.0"
+
+/* Version number of package */
+#define VERSION "1.0"
