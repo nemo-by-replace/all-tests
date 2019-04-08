7689c38c5d18d7e3be5200d02eb7b82c47c43cb6
nemo <zhaopeng026@163.com>
2019-04-08 10:28:32 +0000

    c a test of goto

--- c-c++/practice/goto.c
+++ c-c++/practice/goto.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
-int main(int argc, char **argv)
+
+int main()
 {
-	a = 10;
-
-	if(a = 10) {
+   /* 我的第一个 C 程序 */
+   printf("Hello, World! \n");
+   for(int i = 0; i < 10; i ++)
+   {
+	   loop:
+	   printf("%d\r\n", i);
+   }
 	
-
+	printf("%d\r\n", i);
+	goto loop;
+   return 0;
 }
